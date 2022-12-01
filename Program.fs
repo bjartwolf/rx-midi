6f5bc23893657bae2871a6315d6781f8dec6b493
Bjorn Einar Bjartnes <bjartnes@gmail.com>
2022-12-01 18:30:49 +0100

    arpeggio

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿open NAudio.Midi
+open System
+open FSharp.Control.Reactive
+
+let midiIn = new MidiIn(0)
+let notes = midiIn.MessageReceived |> Observable.filter (fun (x: MidiInMessageEventArgs) -> x.MidiEvent :? NoteEvent)
+                                   |> Observable.map (fun (x: MidiInMessageEventArgs) -> x.MidiEvent :?> NoteEvent) 
+
+let notesOn = notes |> Observable.filter (fun n -> n.CommandCode = MidiCommandCode.NoteOn)
+let output = notesOn |> Observable.bufferCount 3
+let arpeggio = output |> Observable.switchMap (fun x -> Observable.ofSeq(x) |> Observable.zip (Observable.interval (TimeSpan.FromMilliseconds 150)))
+arpeggio |> Observable.subscribe (fun (notes) -> printfn "%A" notes) |> ignore
+midiIn.Start()
+Console.ReadKey() |> ignore
+// https://twitter.com/bjartnes/status/1581229189675778048/photo/1
